4a51b0ecc2e6d5dfae0610bafd266bffbe0b0974
demettriofotografia <demettriofotografia@gmail.com>
2025-12-31 20:08:58 -0300

    Adicionar Footer básico

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer style={{ padding: '30px 20px', textAlign: 'center', backgroundColor: '#222', color: '#fff' }}>
+    © 2025 Malf Mídia. Todos os direitos reservados.
+  </footer>
+);
+
+export default Footer;
